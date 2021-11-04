889b07783a45a6e973b561b370a77b8e4cfe0ba5
Fernanda Dias <fernanda.d_27@hotmail.com>
2021-11-04 16:02:17 -0300

    Ajsutes

--- php_logica/calculoDoces.php
+++ php_logica/calculoDoces.php
@@ -14,44 +14,56 @@ class CalculoDoces
     const BEBIDA_ALCOLICA_NAO_CASAMENTO15ANOS = 2;
     const SOBREMESA_NAO_CASAMENTO15ANOS = 2;
     const SOBREMESA_SIM_CASAMENTO15ANOS = -1;
+    const FESTA_MADRUGADA_SIM = -1;
+    const FESTA_MADRUGADA_NAO = 0;
 
     /** Constantes referentes ao evento aniversario de adulto */
     const ALMOCO_ANIVERSARIO_ADULTO = 4;
     const JANTAR_ANIVERSARIO_ADULTO = 4;
     const FINGER_ANIVERSARIO_ADULTO = 6;
+    const CAFE_ANIVERSARIO_ADULTO = 4;
     const COM_FESTA_ANIVERSARIO_ADULTO = 1;
     const SEM_FESTA_ANIVERSARIO_ADULTO = 0;
     const BEBIDA_ALCOLICA_SIM_ANIVERSARIO_ADULTO = 1;
     const BEBIDA_ALCOLICA_NAO_ANIVERSARIO_ADULTO = 2;
+    const SOBREMESA_NAO_ADULTO = 1;
+    const SOBREMESA_SIM_ADULTO = 0;
 
     /** Constantes referentes aos eventos anversario infantil e batizado */
-    const ALMOCO_INFANTIL_BATIZADO = 4;
-    const CAFE_INFANTIL_BATIZADO = 4;
+    const ALMOCO_INFANTIL_BATIZADO = 3;
+    const CAFE_INFANTIL_BATIZADO = 3;
     const DURACAO_ATE3_INFANTIL_BATIZADO = 0;
     const DURACAO_MAIS3_INFANTIL_BATIZADO = 1;
     const DURACAO_MAIS5_INFANTIL_BATIZADO = 2;
-    const SOBREMESA_NAO_INFANTIL_BATIZADO = 2;
-    const SOBREMESA_SIM_INFANTIL_BATIZADO = -1;
+    const SOBREMESA_NAO_INFANTIL_BATIZADO = 1;
+    const SOBREMESA_SIM_INFANTIL_BATIZADO = 0;
     const BEBIDA_NAO_INFANTIL_BATIZADO = 2;
+    const CONVIDADOS_CRIANÇAS = 1;
+    const CONVIDADOS_ADULTOS = 0;
+    const CONVIDADOS_IGUAIS = 0;
 
     protected $qtdConvidados;
     protected $duracaoEvento;
     protected $outrosDoces;
     protected $bebidas;
     protected $refeicao;
+    protected $idadeConvidados;
+    protected $festaMadrugada;
 
-    public function __construct($qtdConvidados, $duracaoEvento, $outrosDoces, $bebidas, $refeicao)
+    public function __construct($qtdConvidados, $duracaoEvento, $outrosDoces, $bebidas, $refeicao, $idadeConvidados, $festaMadrugada)
     {
         $this->qtdConvidados = $qtdConvidados;
         $this->duracaoEvento = $duracaoEvento;
         $this->outrosDoces = $outrosDoces;
         $this->bebidas = $bebidas;
         $this->refeicao = $refeicao;
+        $this->idadeConvidados = $idadeConvidados;
+        $this->$festaMadrugada = $festaMadrugada;
     }
 
 
 
-    public function comidaServida($refeicao, $const1 = 0, $const2 = 0, $const3 = 0, $const4 = 0)
+    public function comidaServida($refeicao, $const1 = 0, $const2 = 0, $const3 = 0, $const4 = 0, $const5=0)
     {
         if ($refeicao == 'option1') {
             $doces = $const1;
@@ -67,6 +79,10 @@ class CalculoDoces
 
         if ($refeicao == 'option4') {
             $doces = $const4;
+        }
+
+        if ($refeicao == 'option5') {
+            $doces = $const5;
         }
 
 
@@ -119,6 +135,17 @@ class CalculoDoces
         return $doces;
     }
 
+    public function festaMadrugada($festaMadrugada, $const1 = 0, $const2 = 0)
+    {
+        if ($festaMadrugada == 'option1') {
+            $doces = $const1;
+        } else {
+            $doces = $const2;
+        }
+
+        return $doces;
+    }
+
     public function sobremesa($sobremesa, $const1 = 0, $const2 = 0)
     {
         if ($sobremesa == 'option1') {
@@ -141,6 +168,23 @@ class CalculoDoces
         return $doces;
     }
 
+    public function idadeConvidadosInfantil($idadeConvidados, $const1= 0, $const2 = 0, $const3 = 0 )
+    {
+        if ($idadeConvidados == 'option1') {
+            $doces = $const1;
+        }
+
+        if ($idadeConvidados == 'option2') {
+            $doces = $const2;
+        }
+
+        if ($idadeConvidados == 'option3') {
+            $doces = $const3;
+        }
+
+        return $doces;
+    }
+
     public function calculoCasamentoAni15()
     {
         $doces = 0;
@@ -153,6 +197,8 @@ class CalculoDoces
 
         $doces += $this->bebida($this->bebidas, static::BEBIDA_ALCOLICA_SIM_CASAMENTO15ANOS, static::BEBIDA_ALCOLICA_NAO_CASAMENTO15ANOS);
 
+        $doces += $this->festaMadrugada($this->festaMadrugada, static::FESTA_MADRUGADA_SIM, static::FESTA_MADRUGADA_NAO);
+        
         $doces = $doces * $this->qtdConvidados;
 
         return $doces;
@@ -167,6 +213,8 @@ class CalculoDoces
         $doces += $this->duracaoEventoCasamentoFesta15($this->duracaoEvento, static::DURACAO_ATE6_CASAMENTO15ANOS, static::DURACAO_MAIS6_CASAMENTO15ANOS, static::DURACAO_MAIS10_CASAMENTO15ANOS);
 
         $doces += $this->bebida($this->bebidas, static::BEBIDA_ALCOLICA_SIM_CASAMENTO15ANOS, static::BEBIDA_ALCOLICA_NAO_CASAMENTO15ANOS);
+
+        $doces += $this->sobremesa($this->outrosDoces, static::SOBREMESA_SIM_ADULTO, static::SOBREMESA_NAO_ADULTO);
 
         $doces = $doces * $this->qtdConvidados;
 
@@ -184,6 +232,8 @@ class CalculoDoces
         $doces += $this->sobremesa($this->outrosDoces, static::SOBREMESA_NAO_INFANTIL_BATIZADO, static::SOBREMESA_SIM_INFANTIL_BATIZADO);
 
         $doces += $this->bebida($this->bebidas, static::BEBIDA_ALCOLICA_SIM_CASAMENTO15ANOS, static::BEBIDA_NAO_INFANTIL_BATIZADO);
+
+        $doces+=$this->idadeConvidadosInfantil($this->idadeConvidados, static::CONVIDADOS_CRIANÇAS, static::CONVIDADOS_ADULTOS, static::CONVIDADOS_IGUAIS);
 
         $doces = $doces * $this->qtdConvidados;
 
