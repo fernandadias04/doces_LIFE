e5ba289325d189832fc0c8f640316f8711df0ca6
yum1ok <larissamendx2008@gmail.com>
2021-11-30 01:50:32 -0300

    att front

--- Casamento.php
+++ Casamento.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Festa de Casamento</title>
-    <link rel="stylesheet" href="./assets/css/stylesform.css" />
+    <link rel="stylesheet" href="./assets/css/styleform.css" />
 </head>
 
 <body>
